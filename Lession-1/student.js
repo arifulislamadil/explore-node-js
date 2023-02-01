2663c91a67d3b44f088be6241de05786327485f3
Ariful Islam Adil <49348058+arifulislamadil@users.noreply.github.com>
2023-02-01 14:47:22 +0400

    explore node js

---
+++ Lession-1/student.js
@@ -0,0 +1,16 @@
+const getName =()=>{
+    return "Ariful Islam"
+}
+
+
+const getAge =()=>{
+    return "25"
+}
+
+const herAge = 20;
+
+module.exports = {
+    getName,
+    getAge,
+    herAge,
+}
