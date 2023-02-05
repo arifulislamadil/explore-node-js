c1dd24fab47bf696382eac6cded86e8b1ab7f6e5
Ariful Islam Adil <49348058+arifulislamadil@users.noreply.github.com>
2023-02-05 09:41:08 +0400

    explore node js

--- Lession-5/index.js
+++ Lession-5/index.js
@@ -1,8 +1,12 @@
 const http = require("http");
 const port = 5000;
+
 const myServer = http.createServer((req,res)=>{
+    res.writeHead(202,{"Content-Type": "text/html"});
 res.end('<h1> Hello </h1>')
+res.end();
 });
+
 
 myServer.listen(port,(err)=>{
     if(err){
